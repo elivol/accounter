a6b51d90496b1a07cf1314f34e641a030d748643
liza <elivolov@gmail.com>
2022-04-07 11:48:09 +0300

    init commit

---
+++ src/main/java/com/github/elivol/accounter/AccounterApplication.java
@@ -0,0 +1,13 @@
+package com.github.elivol.accounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccounterApplication.class, args);
+	}
+
+}
